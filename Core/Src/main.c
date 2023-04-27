063b19a7242c114eecfa3e4253dd633da856c954
LuckyMario <55717820+MarioGeier00@users.noreply.github.com>
2023-04-27 16:53:44 +0200

    adds bit structure comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -627,7 +627,9 @@ void lcd_send_cmd (char cmd)
 	data_u = (cmd&0xf0);
 	data_l = ((cmd<<4)&0xf0);
 	data_t[0] = data_u|0x0C;  //en=1, rs=0
+  //                00001100 
 	data_t[1] = data_u|0x08;  //en=0, rs=0
+  //                    1000
 	data_t[2] = data_l|0x0C;  //en=1, rs=0
 	data_t[3] = data_l|0x08;  //en=0, rs=0
 	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, 100);
@@ -639,7 +641,9 @@ void lcd_send_data (char data)
 	data_u = (data&0xf0);
 	data_l = ((data<<4)&0xf0);
 	data_t[0] = data_u|0x0D;  //en=1, rs=1
-	data_t[1] = data_u|0x09;  //en=0, rs=1
+  //                    1101
+  data_t[1] = data_u|0x09;  //en=0, rs=1
+  //                    1001
 	data_t[2] = data_l|0x0D;  //en=1, rs=1
 	data_t[3] = data_l|0x09;  //en=0, rs=1
 	HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, 100);
@@ -659,6 +663,8 @@ void lcd_init (void)
 
   // dislay initialisation
 	lcd_send_cmd (0x28); // Function set --> DL=0 (4 bit mode), N = 1 (2 line display) F = 0 (5x8 characters)
+  //           00101000
+  //           001DNFxx
 	HAL_Delay(1);
 	lcd_send_cmd (0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
 	HAL_Delay(1);
